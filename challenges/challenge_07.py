7e0c7d1127a525ce58d1ec45c27e32b2841c2b69
George Finn <george.charles.finn@gmail.com>
2025-07-01 20:20:44 +0100

    Added ECB Encryption Test (Penguin)

--- challenges/challenge_07.py
+++ challenges/challenge_07.py
@@ -11,6 +11,8 @@ import os
 
 from base64 import b64decode
 from Crypto.Cipher import AES
+from Crypto import Random
+from PIL import Image
 
 AES_KEY = b"YELLOW SUBMARINE"  # Fixed 16-byte key for AES in ECB mode
 
@@ -53,8 +55,45 @@ def run_challenge(input_data: str):
     else:
         print("❌ Decryption did not match expected result.")
 
+    print("🖼️ Bonus: Encrypting the penguin image...")
+
+    image = Image.open(f"{os.getcwd()}/challenges/assets/penguin.png").convert('RGBA').convert('RGB')
+    key = Random.new().read(AES.key_size[0])  # Generate a random key of the appropriate size
+
+    print("🔒 Encrypting image in ECB mode...")
+    encrypt_image(image, key, AES.MODE_ECB).save(
+        f"{os.getcwd()}/challenges/assets/encrypted_penguin_ecb.png"
+    )
+
+    print("🔒 Encrypting image in CBC mode...")
+    iv = Random.new().read(AES.block_size)
+    encrypt_image(image, key, AES.MODE_CBC, iv).save(
+        f"{os.getcwd()}/challenges/assets/encrypted_penguin_cbc.png"
+    )
+
 
 def aes_ecb_decrypt(ciphertext: bytes, key: bytes) -> bytes:
     """Decrypt ciphertext using AES in ECB mode with the given key."""
     cipher = AES.new(key, AES.MODE_ECB)
     return cipher.decrypt(ciphertext)
+
+
+def encrypt_image(image: Image, key: bytes, mode: int = AES.MODE_ECB, iv: bytes = b'') -> Image:
+    image_array = bytes(image.tobytes())
+    padding_length = AES.block_size - len(image_array) % AES.block_size
+    image_array += bytes(padding_length * ".", "UTF-8")  # Just an arbitrary padding byte
+
+    if mode == AES.MODE_CBC and not iv:
+        raise ValueError("IV must be provided for CBC mode")
+    elif mode not in (AES.MODE_ECB, AES.MODE_CBC):
+        raise ValueError("Unsupported AES mode. Use AES.MODE_ECB or AES.MODE_CBC.")
+    
+    if mode == AES.MODE_CBC:
+        aes = AES.new(key, mode, iv)
+    else:
+        aes = AES.new(key, mode)
+
+    encrypted_image = aes.encrypt(image_array)
+    encrypted_image = encrypted_image[:-padding_length]
+
+    return Image.frombytes("RGB", image.size, encrypted_image, "raw", "RGB")
